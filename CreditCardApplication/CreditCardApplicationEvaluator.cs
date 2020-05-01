032e0f31728a78433cde182e9f29b004278cf518
mndiaye00 <msanes13@gmail.com>
2020-05-01 15:17:01 -0400

    Add CreditCardApplication.cs, CreditCardApplicationDecision.cs and CreditCardApplicationEvaluator.cs classes

---
+++ CreditCardApplication/CreditCardApplicationEvaluator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCardApplication
+{
+    public class CreditCardApplicationEvaluator
+    {
+        private const int AutoReferralMaxAg = 20;
+        private const int HighIncomeThreshhold = 100_000;
+        private const int LowIncomeThreshhold = 20_000;
+
+        public CreditCardApplicationDecision Evaluate(CreditCardApplication application)
+        {
+            if (application.GrossAnnualIncome >= HighIncomeThreshhold)
+            {
+                return CreditCardApplicationDecision.AutoAccepted;
+            }
+
+            if(application.Age <= AutoReferralMaxAg)
+            {
+                return CreditCardApplicationDecision.ReferedToHuman;
+            }
+
+            if (application.GrossAnnualIncome < LowIncomeThreshhold)
+            {
+                return CreditCardApplicationDecision.AutoDeclined;
+            }
+
+            return CreditCardApplicationDecision.ReferedToHuman;
+        }
+
+    }
+}
