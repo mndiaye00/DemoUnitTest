0221a68894b5de92dcb3e61e5103b9efa4def4d9
mndiaye00 <msanes13@gmail.com>
2020-05-01 12:52:39 -0400

    Add PlayerCharacter and PlayerCharacterTest class with update QuickApp.csproj

---
+++ QuickApp.Test/PlayerCharacterTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace QuickApp.Test
+{
+    public class PlayerCharacterTest
+    {
+        [Fact]
+        private void ShouldHaveDefaultRandomGenerateName()
+        {
+            var sut = new PlayerCharacter();
+
+            var result = sut.Name;
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        private void ShouldNotHaveANickName()
+        {
+            var sut = new PlayerCharacter();
+
+            var result = sut.NickName;
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        private void ShouldBeNewbie()
+        {
+            var sut = new PlayerCharacter();
+
+            var result = sut.IsNoob;
+
+            Assert.True(result);
+        }
+    }
+}
---
+++ QuickApp/PlayerCharacter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickApp
+{
+    public class PlayerCharacter
+    {
+        public string Name { get; private set; }
+
+        public bool IsNoob { get; set; }
+
+        public string NickName { get; set; }
+
+        public int Health { get; set; }
+
+        public List<string> Weapons { get; set; }
+
+        public PlayerCharacter()
+        {
+            Name = GenerateName();
+
+            IsNoob = true;
+
+            CreateStartingWeapons();
+        }
+
+        private void CreateStartingWeapons()
+        {
+        }
+
+        private string GenerateName()
+        {
+            var names = new[]
+            {
+                "Diari",
+                "Moussa",
+                "Aboubacr",
+                "Fatima", 
+                "Khadidia"
+            };
+
+            return names[new Random().Next(0, names.Length)];
+        }
+    }
+}
